1e195a773f42278ee7620466326d8ae3cada9f45
Maicol <maicol.oficial.93@gmail.com>
2025-02-25 12:37:24 -0400

    Modify environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,8 @@ export const environment = {
     urlBaseSUPA: 'https://feppffmvlbpotolkqmls.supabase.co/rest/v1/',
     apiKeyCOC: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFhNDEwNjIwLWRmYjctNDVlMC1iNDZkLTE0MmEzOTcyNmU4YyIsImlhdCI6MTY5ODcwNDIzMCwic3ViIjoiZGV2ZWxvcGVyLzA1MDVmZGFjLTgyMzYtMjMyMy1kOWI1LTJlNGFjMjYyZDE2ZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.vKA6X0SCEBGcoSoLFKT1bLfF2XYR9L0YuUccFun7xeyQ5E7Xm29p6C-FpH6fI1CHG_ucj6QuxD_KQJ6Bcd7B9A',
     apiKeySUPA: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlcHBmZm12bGJwb3RvbGtxbWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5OTYzMzgsImV4cCI6MjA1NTU3MjMzOH0.JR0bHGB4o2rDaLDJ4aIsjQhrb33-VyBhEUU57ZQnlT8',
-    urlBaseBack: 'https://clash-with-expressjs.vercel.app/v1/'
+    // urlBaseBack: 'https://clash-with-expressjs.vercel.app/v1/'
+    urlBaseBack: 'https://clash-backend-2.vercel.app/v1/'
     // urlBaseBack: 'http://localhost:3000/v1/'
 };
 
