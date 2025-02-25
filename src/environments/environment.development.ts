10b4817fc1500f57a6ee98e5889bcc381e82b86f
Maicol <maicol.oficial.93@gmail.com>
2025-02-25 19:22:06 -0400

    Modify for prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     urlBaseSUPA: 'https://feppffmvlbpotolkqmls.supabase.co/rest/v1/',
     apiKeySUPA: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlcHBmZm12bGJwb3RvbGtxbWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5OTYzMzgsImV4cCI6MjA1NTU3MjMzOH0.JR0bHGB4o2rDaLDJ4aIsjQhrb33-VyBhEUU57ZQnlT8',
-    // urlBaseBack: 'https://clash-backend-2.vercel.app/v1/'
-    urlBaseBack: 'http://localhost:3000/v1/'
+    urlBaseBack: 'https://clash-backend-2.vercel.app/v1/'
+    // urlBaseBack: 'http://localhost:3000/v1/'
 };
 
 // https://cocproxy.royaleapi.dev/v1/clans/%23RUG0LC2Q/currentwar guerra actual
